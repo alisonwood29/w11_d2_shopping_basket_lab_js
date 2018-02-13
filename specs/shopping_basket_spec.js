1f1751cbaa970ed5f7ade3ddc8c251e9ea0e4d14
Alison Wood <alisonwood29@gmail.com>
2018-02-13 15:54:30 +0000

    added test for removing item

--- specs/shopping_basket_spec.js
+++ specs/shopping_basket_spec.js
@@ -9,6 +9,8 @@ describe('shopping basket', function () {
 
   beforeEach(function () {
     shoppingBasket = new ShoppingBasket();
+    item1 = new Item('beer', 2.00, false);
+    item2 = new Item('gin', 12.00, true);
   });
 
   it('should have items', function () {
@@ -25,6 +27,14 @@ describe('shopping basket', function () {
     const actual = shoppingBasket.getItemCount();
     assert.strictEqual(actual, 1);
   });
+
+  it('should be able to remove an item', function(){
+    shoppingBasket.addItem(item1);
+    shoppingBasket.addItem(item2);
+    shoppingBasket.removeItem(item1);
+    const actual = shoppingBasket.items;
+    assert.deepStrictEqual(actual, [item2])
+  })
 
 
 
